42579120b03dfa54885f20284354fa7071f8d903
Daniel Marten <78616802+matren395@users.noreply.github.com>
2022-04-03 14:48:34 -0500

    Add files via upload
    Adding my unique Run & Parse Taxonomized BLAST script for this project. It is VERY fast not not very clean, but it was primarily just included to speed up the process.

---
+++ runParse.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+
+$inputFasta = $ARGV[0]; # no usage statement I am lazy
+$outputName = $ARGV[1];
+
+system("perl ~/projectSymLinkT02/scripts/runTaxonomizedBLAST.pl --threads 8 -p blastn -a megablast -d /media/Data_1/NCBI/REPGENOMES/ref_prok_rep_genomes --query $inputFasta --evalue 1e-5 --culling 5");
+# system("perl ~/projectSymLinkT02/scripts/parseTaxonomizedBLAST.pl --blast *blastn.6 -f $inputFasta --name Escherichia \"Escherichia\" --evalue 1e-50 --output $outputName -v on");
+system("perl ~/projectSymLinkT02/scripts/parseTaxonomizedBLAST.pl -b *blastn.6 -f $inputFasta -n Escherichia \"Escherichia\" --evalue 1e-50 --output $outputName -v on");
