d19e4bfe84628e69d7a1050309bafdd9a51b7d88
Daniel Marten <78616802+matren395@users.noreply.github.com>
2022-04-03 14:49:11 -0500

    Update runParse.pl

--- runParse.pl
+++ runParse.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-$inputFasta = $ARGV[0]; # no usage statement I am lazy
+$inputFasta = $ARGV[0]; # no usage statement, usage covered in PDF report 
 $outputName = $ARGV[1];
 
 system("perl ~/projectSymLinkT02/scripts/runTaxonomizedBLAST.pl --threads 8 -p blastn -a megablast -d /media/Data_1/NCBI/REPGENOMES/ref_prok_rep_genomes --query $inputFasta --evalue 1e-5 --culling 5");
